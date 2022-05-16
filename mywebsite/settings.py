32f2f27d2710f0376b39d4ef3375fcab627b7313
M Rahman <maurahman@hotmail.com>
2022-05-16 20:43:28 +0100

    update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a=7xc6697re+d)_3ef((u9y_h&74*^647-#sg^s8j#in88o-^r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'codingconfig.com']
 
